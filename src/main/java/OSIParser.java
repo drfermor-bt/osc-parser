d0f16dcf461fb08e6c93f46d9bdf5099115c3bea
Duncan Fermor <duncan.fermor@bt.com>
2019-09-27 13:55:05 +0100

    replace Vector with ArrayList

--- src/main/java/OSIParser.java
+++ src/main/java/OSIParser.java
@@ -1,13 +1,22 @@
+import java.util.ArrayList;
 import java.util.Vector;
 
 public class OSIParser {
     public static void main(String[] args) throws java.io.IOException {
-        byte[] verbBytes = new byte[4];
-        System.in.read(verbBytes);
-        String verb = new String(verbBytes);
-        System.out.printf("Verb: '%s'\n", verb);
+        int verbSize = 4;
 
-        Vector<Integer> parseInstructions = new Vector<Integer>();
+        // Verb
+        byte[] verbBytes = new byte[verbSize];
+        System.in.read(verbBytes);
+        StringBuilder verbBuilder = new StringBuilder();
+        for (int i = 0; i < verbSize; i++) {
+            byte b = verbBytes[i];
+            verbBuilder.append((char) b);
+        }
+        System.out.printf("Verb: '%s'\n", verbBuilder);
+
+        // Tags
+        ArrayList<Integer> parseInstructions = new ArrayList<Integer>();
         byte[] current = new byte[1];
         int last = System.in.read(current);
         while(last > -1) {
@@ -19,10 +28,11 @@ public class OSIParser {
             last = System.in.read(current);
         }
 
-        System.out.printf("Tags: '%s'\n", parseInstructions);
+        // Payload
+        System.out.printf("Tags: %s\n", parseInstructions);
         int tag;
         for (int i = 0; i < parseInstructions.size(); i++) {
-            tag = parseInstructions.elementAt(i);
+            tag = parseInstructions.get(i);
             if (tag == 105) {
                 System.in.read(current);
                 System.out.printf("Attribute %d (int32): %s\n", i, current[0]);

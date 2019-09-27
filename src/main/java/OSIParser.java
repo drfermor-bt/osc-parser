663880aab8976d9ce70978bdbe0c71edbb04e575
Duncan Fermor <duncan.fermor@bt.com>
2019-09-27 13:37:43 +0100

    refactoring post-DeepCode

--- src/main/java/OSIParser.java
+++ src/main/java/OSIParser.java
@@ -1,31 +1,32 @@
+import java.util.Vector;
+
 public class OSIParser {
     public static void main(String[] args) throws java.io.IOException {
         byte[] verbBytes = new byte[4];
         System.in.read(verbBytes);
         String verb = new String(verbBytes);
-        System.out.printf("Verb: '%s'", verb);
+        System.out.printf("Verb: '%s'\n", verb);
 
-        StringBuilder tagBuilder = new StringBuilder();
-
+        Vector<Integer> parseInstructions = new Vector<Integer>();
         byte[] current = new byte[1];
         int last = System.in.read(current);
         while(last > -1) {
             if (current[0] == 0x00) {
                 break;
             }
-            tagBuilder.append(current);
+            parseInstructions.add((int) current[0]);
 
             last = System.in.read(current);
         }
 
-        System.out.printf("Tags: '%s'", tagBuilder);
-
-        for (int i = 0; i < tagBuilder.length(); i++) {
-            char tag = tagBuilder.charAt(i);
-            if (tag == 'i') {
+        System.out.printf("Tags: '%s'\n", parseInstructions);
+        int tag;
+        for (int i = 0; i < parseInstructions.size(); i++) {
+            tag = parseInstructions.elementAt(i);
+            if (tag == 105) {
                 System.in.read(current);
-                System.out.printf("Attribute %d (int32): %s", i, current[0]);
-            } else if (tag == 's') {
+                System.out.printf("Attribute %d (int32): %s\n", i, current[0]);
+            } else if (tag == 115) {
                 StringBuilder attributeBuilder = new StringBuilder();
                 current = new byte[1];
                 last = System.in.read(current);
@@ -33,11 +34,11 @@ public class OSIParser {
                     if (current[0] == 0x00) {
                         break;
                     }
-                    attributeBuilder.append(current);
+                    attributeBuilder.append(Character.toString(current[0]));
 
                     last = System.in.read(current);
                 }
-                System.out.printf("Attribute %d (int32): %s", i, attributeBuilder);
+                System.out.printf("Attribute %d (string): %s\n", i, attributeBuilder);
             }
         }
     }
